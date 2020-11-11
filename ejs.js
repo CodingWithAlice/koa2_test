c059c44a4ae5590408213b39c4654ed1e5ea0e7c
CodingWithAlice <2269131254@qq.com>
2020-11-11 11:31:15 +0800

    koa2中模版引擎渲染数据到静态页面+ejs的公共数据ctx.state

--- ejs.js
+++ ejs.js
@@ -2,7 +2,7 @@
  * @Author: Alice 
  * @Date: 2020-11-10 17:47:16 
  * @Last Modified by: ling.hao
- * @Last Modified time: 2020-11-11 10:03:27
+ * @Last Modified time: 2020-11-11 11:27:14
  * @Description: {'ejs模版的使用'} 
  */
 
@@ -23,17 +23,33 @@ app.use(views('views',{
 // }}));
 
 router.get('/', async (ctx) => {
-    // ctx.body = 'hello koa2';
     await ctx.render('index');
 })
 
 router.get('/star', async (ctx) => {
-    ctx.body = 'hello 千玺';
+    let title = '千玺 二十岁快乐';
+    let arr = ['恒温动物','my boo', '精彩才刚刚开始'];
+    let html = `<h2>这是一个html数据</h2>`;
+    await ctx.render('index',{
+        title,
+        list: arr,
+        html,
+        num: 15,
+    });
 })
 
 router.get('/cat', async (ctx) => {
     ctx.body = 'hello 阿秋';
 })
+
+// 需求：在每个路由 render 里面都渲染一个公共数据 - ctx.state
+app.use(async (ctx, next) => {
+    ctx.state = {
+        name: '千纸鹤',
+    }
+    await next();
+});
+
 // 使用【中间件】启动路由
 app
   .use(router.routes())
